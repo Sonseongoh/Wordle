4e8687b19f5c4af1ef7d2fbd3456db53c2117b94
Sonseongoh <nobleser@naver.com>
2023-11-05 23:55:29 +0900

    wordle 게임

---
+++ main.js
@@ -0,0 +1,26 @@
+let Answer="abcde"
+document.querySelector('button').addEventListener('click',
+function(){
+    let input =document.querySelectorAll('.input');
+
+    for(let i=0; i<5; i++){
+
+        if(input[i].value ==Answer[i]){
+            input[i].style.background = "green"
+        }else if(Answer.includes(input[i].value)){
+            input[i].style.background = "yellow"
+        }else{
+            input[i].style.background = "red"
+    }}
+
+let template = `<div>
+    <input class="input">
+    <input class="input">
+    <input class="input">
+    <input class="input">
+    <input class="input">
+</div>`
+document.querySelector('body').insertAdjacentHTML
+('beforeend',template)
+
+})
